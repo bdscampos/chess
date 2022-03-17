a8a7321d95b8a4a35a5afc83e2ce7600c97c90ec
Bruno Campos <bdscampos@gmail.com>
2022-03-17 20:25:09 -0300

    Criada classe Posicao

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,10 @@
+package aplicacao;
+
+import Tabuleiro.Posicao;
+
+public class Programa {
+    public static void main(String[] args) {
+        Posicao p = new Posicao(3,5);
+        System.out.println(p);
+    }
+}
